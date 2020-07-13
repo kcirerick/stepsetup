7d5e648c3f4e23f4c975af4f42ad3a6347799e5a
Erick <enriquezerick182@gmail.com>
2020-07-13 20:39:57 +0000

    FindMeetingQuery passes first two tests.

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,16 @@
 
 package com.google.sps;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    Collection<TimeRange> options = Arrays.asList();
+    if(request.getDuration() > TimeRange.WHOLE_DAY.duration()) return options;
+    if(events.isEmpty()) return Arrays.asList(TimeRange.WHOLE_DAY);
   }
 }
